27641f057c2c537abe4bd74cb371503e4c53bda3
Tushar98644 <b.toshar@iitg.ac.in>
2023-04-23 01:58:51 +0530

    typo

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,8 +2,11 @@ const user = require('../models/User');
 
 const RegisterUser = async(req, res) => {
     try {
-        const newUser = new user(
-            req.body
+        const newUser = new user({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        }
         );
         await newUser.save();
         res.status(200).json({ message: 'User created successfully' });
